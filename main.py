946ca8284cf4d7f9079e5d6e14e1e69a447eab78
KateZin <90635745+KateZin@users.noreply.github.com>
2024-10-14 23:43:14 +0300

    add to-do comments

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from DatasetProcessor import DatasetManager
-
+import time
 
 # TODO сбилдить фичи для сравнения маскированных объектов и фона вцелом
 # TODO добавить медианную яркость
@@ -9,12 +9,18 @@ from DatasetProcessor import DatasetManager
 # проверить нули в таблицах
 # карта градиента? объединить
 
-# добавить контуры в инфо
+
 # добавить обработку ошибок: конциг ридер
+# заменить все на подсчет numpy
+# добавить сохранение мета файла и построение по мета файлу
+# добавить описание фичей
+# добавить паралельное вычисление
+# убрать лог с исключением фичей
 if __name__ == "__main__":
+    start = time.time()
     manager = DatasetManager.DatasetManager("./config/config.yaml")
     manager.run_()
-
+    print(time.time() - start)
 
 
 
